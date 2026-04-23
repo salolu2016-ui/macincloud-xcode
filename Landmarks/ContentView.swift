94b420269cfd0b87301beb6015dd2fbec8424eec
Diany <salolu2016@gmail.com>
2026-04-23 00:02:47 -0500

    ...

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -4,18 +4,20 @@
 //
 //  Created by user286595 on 4/22/26.
 //
+// Diana Luna
+// Diplomado de Diseño y Programación de Apps
+// ING2450 - Programación de Apps para IOS - (A73)
+// 26/04/2026
+// Ordenamiento de una lista de números
+//Reto de aprendizaje 2. App Mobile: Landmarks
+//
 import SwiftUI
 
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("Bienvenida Diany!")
+      
     }
 }
 
@@ -23,4 +25,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-    
