a40dabe1044b1f9aa395352de05186aebac800a8
Diany <salolu2016@gmail.com>
2026-04-22 19:58:43 -0500

    Primer commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by user286595 on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
