1f39a48021f3830f19603f1f1e160fd6052341ed
Diany <salolu2016@gmail.com>
2026-04-23 23:38:29 -0500

    Insertar imagen

---
+++ Landmarks/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  Landmarks
+//
+//  Created by user286595 on 4/23/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
