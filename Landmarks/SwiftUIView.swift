ea9a1941fdc3400494c652cbab23f05b0775a92e
Diany <salolu2016@gmail.com>
2026-04-23 23:57:59 -0500

    Borde blanco imagen

--- Landmarks/SwiftUIView.swift
+++ Landmarks/SwiftUIView.swift
@@ -11,7 +11,10 @@ struct SwiftUIView: View {
     var body: some View {
         Image("turtlerock")
             .clipShape(Circle())
-    }
+            .overlay {
+                            Circle().stroke(.white, lineWidth: 4)
+                      }
+            .shadow(radius: 7)    }
 }
 
 #Preview {
