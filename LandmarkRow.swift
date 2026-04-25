995cae0bdd46a7e3b89ac740ca4bd89875372569
Diany <salolu2016@gmail.com>
2026-04-24 20:30:39 -0500

    Matriz de lugares e imagenes

--- LandmarkRow.swift
+++ LandmarkRow.swift
@@ -4,6 +4,14 @@
 //
 //  Created by user286595 on 4/24/26.
 //
+//
+// Diana Luna
+// Diplomado de Diseño y Programación de Apps
+// ING2450 - Programación de Apps para IOS - (A73)
+// 26/04/2026
+// Ordenamiento de una lista de números
+//Reto de aprendizaje 2. App Mobile: Landmarks
+//
 
 import SwiftUI
 
