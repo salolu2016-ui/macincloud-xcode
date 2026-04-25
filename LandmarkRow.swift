83e4a7f422ed4e54d17fc1e6aa256ba75ffd5d87
Diany <salolu2016@gmail.com>
2026-04-25 12:35:17 -0500

    Marca estrella de favoritos

--- LandmarkRow.swift
+++ LandmarkRow.swift
@@ -28,6 +28,13 @@ struct LandmarkRow: View {
             Text(landmark.name)
             
             Spacer()
+            
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+                
+            }
         }
     }
 }
